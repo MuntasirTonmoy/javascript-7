9711f9e596264bdc5f700f7da2f29a4a4ee19e9e
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-04 23:08:49 +0600

    lecture-7 javascript practice problem

---
+++ lecture-1-math-and-random-number.js
@@ -0,0 +1,17 @@
+// use 5 basic concept of js
+// TODO search Javascript Math https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+var Mathfunction1 = -5;
+console.log(Math.abs(Mathfunction1)); // Math e M uppercase
+// here Math.abs() return the absolute value
+
+var Mathfunction = 12.66;
+console.log(Math.ceil(Mathfunction)); // celing decimal point er ager songkha return kore
+
+console.log(Math.floor(Mathfunction)); // decimal point er ager songkha return kore
+
+console.log(Math.round(Mathfunction)); //.5 er niche hole ager songkha return kore r .5 er beshi hole porer songkha
+
+console.log(Math.random() * 6); // 0-1 er moddhe random songkha de er sathe number gun dile oi number porjonto songkha dekhay . decimal point er porer songkha remove korte chaile Math.celi or floor or round use korte hobe
+
+console.log(Math.ceil(Math.random() * 6)); 
---
+++ practice.js
@@ -0,0 +1,5 @@
+// find the smallest number of an array
+// find the smallest number among three numbers
+// make a function that can input an array of numbers and then calculate the average of them then return average
+// make a function that can input length and width of an rectangle and then return the area of it.
+// find the second largest number of an array
