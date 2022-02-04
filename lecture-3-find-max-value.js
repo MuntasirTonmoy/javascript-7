84dc3235415a30b2773815c6b991d7d41d6ddaaa
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-04 23:54:07 +0600

    smallest number among three numbers

--- lecture-3-find-max-value.js
+++ lecture-3-find-max-value.js
@@ -27,3 +27,17 @@ console.log(maxThree(8, 45, 56));
 // we can also find the max value by using Math.max function
 
 console.log('largest is', Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9)); // it will return the max value;
+
+//task write a function to find the smallest number among three numbers
+function minThree(num1, num2, num3) {
+    if (num1 < num2 && num1 < num3) {
+        return num1;
+    }
+    else if (num2 < num3) {
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+console.log('the smallest number among three is ', minThree(8, 45, 56));
